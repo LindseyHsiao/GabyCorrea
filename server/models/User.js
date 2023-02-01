8fa9e45431345e822ee173a92e63a56f4db3e002
Lindsey Sherrill <lindsey350@aol.com>
2023-02-01 15:27:45 -0500

    server.js and .gitignore updated added user model

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');  //Schema constuctor, model function
+
+const UserSchema = new Schema({
+    userName: {
+        type: String
+    }
+})
+
+//create the User model using the UsersSchema
+const User = model('User', UserSchema);
+
+//export the User model
+module.exports = Pizza;
