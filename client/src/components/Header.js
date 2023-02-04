710bf6a18c1bd5ea6b0a6621455c5c901b63e382
Lindsey Sherrill <lindsey350@aol.com>
2023-02-03 23:09:24 -0500

    updated header home and logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,62 +1,36 @@
-import janJenningLogo from '../assets/images/JanJennings.png'
+import logo from '../assets/images/gcLogo.png'
 
 import {useState} from 'react';
 export default function Header() {
 const [toggle, setToggle] = useState(false)
 
     return (
-        <section className="bg-gradient-to-b from-blue-50 to-sky-500">
-        {/* <!-- Hero --> */}
-        <div className="container max-w-6xl mx-auto px-6 py-12">
-            {/* <!-- Menu/Logo Container --> */}
-            <nav className="flex items-center justify-between font-bold text-white">
-                {/* <!-- Logo --> */}
-                <a href="/"><img className="" src={janJenningLogo} alt="Jan Jennings logo" /></a>
-                {/* <!-- Menu --> */}
-                <div className="hidden h-10 md:flex md:space-x-8">
-                    <div className="group">
-                        <a href="/Contact">About</a>
-                        <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+        <>
+    
+        <nav className=" bg-orange-50">
+            {/* <!-- Flex container for all items --> */}
+            <div className="flex items-center px-10 py-2 justify-between">
+                {/* <!-- Flex container for logo and left menu --> */}
+                <div className="flex items center space-x-10">
+                    {/* <!-- Logo --> */}
+                    <img src={logo} alt="" className='h-10'/>
+                    {/* <!-- Left menu --> */}
+                    <div className="hidden pt-1 space-x-10 font-bold lg:flex">
+                        <a href="#" className="hover:opacity-70 uppercase">About</a>
+                        <a href="#" className="hover:opacity-70 uppercase">Testimonies</a>
+                        <a href="#" className="hover:opacity-70 uppercase">Contact</a>
                     </div>
-                    <div className="group">
-                        <a href="/Events">Testimonies</a>
-                        <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
-                    </div>
-                    <div className="group">
-                        <a href="/Lessons">Contact</a>
-                        <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
-                    </div>
-                    
-                    {/* button container */}
-                <div className="group">
-                    <a href="/Contact" className="btn my-10 ">Log In</a>
                 </div>
-                {/* button container */}
-                <div className="group">
-                    <a href="/Contact" className="btn my-10 ">Sign Up</a>
+    
+                {/* <!-- Right buttons menu --> */}
+                <div className="hidden items-center space-x-6 font-bold lg:flex">
+                    <div className="hover:opacity-70 uppercase">Login</div>
+                    <a href="#" className="px-3 py-2 font-bold text-white bg-black rounded hover:opacity-70 uppercase">Sign Up</a>
                 </div>
-
-                </div>
-                {/* <!-- hamburger button  --> */}
-                <div className="md:hidden">
-                    <button id="menu-btn" type="button" className={toggle ? "z-40 block hamburger md:hidden focus:outline-none open" : "z-40 block hamburger md:hidden focus:outline-none"} onClick={()=> setToggle(!toggle)}>
-                        <span className="hamburger-top"></span>
-                        <span className="hamburger-middle"></span>
-                        <span className="hamburger-bottom"></span>
-                    </button>
-                </div>
-            </nav>
-            {/* <!-- mobile menu --> */}
-            <div id="menu" className={toggle ? "absolute top-0 bottom-0 left-0 flex flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black" : "absolute top-0 bottom-0 left-0 hidden flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"}>
-                <a href="/Contact" className="hover:text-pink-500">About</a>
-                <a href="/Events" className="hover:text-pink-500">Testimonies</a>
-                <a href="/Lessons" className="hover:text-pink-500">Contact</a>
-                <a href="/Listen" className="hover:text-pink-500">Listen</a>
-                <a href="/About" className="hover:text-pink-500">About</a>
+                {/* <!-- todo:hamburger menu --> */}
             </div>
-
-            
-        </div>
-    </section>
+            {/* <!-- todo: mobile menu --> */}
+        </nav>
+        </>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,14 @@
-
+import bwBarLift from '../assets/images/bwBarLift.jpg'
 
 export default function Home() {
     return (
         <>
-        <h1 class="underline bg-gray-400">Hello</h1>
+        <div id='home-page-img'>
+        <img src={bwBarLift} alt='' className='max-w-full h-auto'></img>
+        </div>
+        <div className="bg-orange-50 h-40 w-auto">
+            <p>Strengthen from the inside out</p>
+        </div>
         
         </>
     )
