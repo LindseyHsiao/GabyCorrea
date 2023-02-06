2571134dcc89d3605bb5f80d7a8458bb4667c09d
Lindsey Sherrill <lindsey350@aol.com>
2023-02-05 22:50:44 -0500

    added features section to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
                 <img src={bwBarLift} alt='' className='max-w-full h-auto'></img>
             </div>
 
-            <section id="strengthen" class="py-36 bg-orange-50">
+            <section id="strengthen" className="py-36 bg-orange-50">
                 <div className="container mx-auto px-3">
                     <h2 className="text-4xl mb-6 font-bold text-center uppercase">
                         Strengthen from the inside out
@@ -19,33 +19,97 @@ export default function Home() {
                 </div>
             </section>
 
-            <section id="contact-details" >
-                <div className="section-container contact">
-                    <div className="relative flex flex-col md:flex-row md:space-x-32">
-                        {/* image */}
-                        <div className="md:w-1/2">
-                            <img src={sand} alt="sand" className="md:absolute top-0 right-[50%]" />
-                        </div>
-                        {/* items container */}
-                        <div className="flex flex-col mt-16 mb-24 space-y-12 text-xl md:w-1/2 md:mb-60 md:text-left md:pl-16">
-                            {/* item 1 */}
-                            <div>
-                                <h4 className="uppercase text-center">get the app</h4>
-                                
+
+
+            {/* <!-- Feature Box Section --> */}
+            <section id="features" className="pb-32 bg-orange-50">
+                <div className="relative container flex flex-col items-center px-6 mx-auto md:flex-row md:space-x-7">
+                    {/* <!-- Horizontal Line --> */}
+                    <div className="hidden absolute top-24 w-10/12 left-16 h-1 bg-black md:block"></div>
+                    {/* <!-- Vertical Line --> */}
+                    <div className="absolute w-1 left-1/2 h-full -ml-1 bg-black md:hidden"></div>
+                    {/* <!-- Box 1 --> */}
+                    <div className="relative flex flex-col p-6 space-y-6 bg-white rounded md:w-1/3">
+                        {/* <!-- Image Positioning --> */}
+                        <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+                            {/* <!-- Image container for background and center --> */}
+                            <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-black">
+                                <img src="images/icon-brand-recognition.svg" alt="" />
                             </div>
-                            {/* item 2 */}
-                            <div>
-                                
-                            <a href='#' className="max-w-md opacity-70 italic">access workouts</a>
-                            </div>
-                            
                         </div>
+                        <h5 className="pt-6 text-xl font-bold text-center uppercase md:text-left">
+                            Nutrition
+                        </h5>
+                        <p className="text-center opacity-70 md:text-left">
+                        Generic links don't mean a thing. Branded links help instill confidence in your concept.
+                        </p>
+                    </div>
+
+                    {/* <!-- Box 2 --> */}
+                    <div className="relative flex flex-col mt-24 p-6 space-y-6 bg-white rounded md:mt-0 md:w-1/3">
+                        {/* <!-- Image Positioning --> */}
+                        <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+                            {/* <!-- Image container for background and center --> */}
+                            <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-black">
+                                <img src="images/icon-detailed-records.svg" alt="" />
+                            </div>
+                        </div>
+                        <h5 className="pt-6 text-xl font-bold text-center uppercase md:text-left">
+                            Workouts
+                        </h5>
+                        <p className="text-center opacity-70 md:text-left">
+                            Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.
+                        </p>
+                    </div>
+
+                    {/* <!-- Box 3 --> */}
+                    <div className="relative flex flex-col mt-24 p-6 space-y-6 bg-white rounded md:mt-0 md:w-1/3">
+                        {/* <!-- Image Positioning --> */}
+                        <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+                            {/* <!-- Image container for background and center --> */}
+                            <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-black">
+                                <img src="images/icon-fully-customizable.svg" alt="" />
+                            </div>
+                        </div>
+                        <h5 className="pt-6 text-xl font-bold text-center uppercase md:text-left">
+                            Results
+                        </h5>
+                        <p className="text-center opacity-70 md:text-left">
+                            Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.
+                        </p>
                     </div>
                 </div>
             </section>
 
-
-
+            
+            <section id="contact-details">
+        <div className="section-container contact">
+            <div className="relative flex flex-col md:flex-row md:space-x-32">
+                {/* image */}
+                <div className="md:w-1/2">
+                    <img src={sand} alt="Jan" className="md:absolute top-0 right-[40%]"/>
+                </div>
+                {/* items container */}
+                <div className="flex flex-col mt-16 mb-24 space-y-12 text-xl md:w-2/5 md:mb-10 md:text-left md:pl-36">
+                    {/* item 1 */}
+                    <div>
+                        <h4>email</h4>
+                        <a href='mailto:mail@harpbiz.com' className="max-w-md text-gray-700">mail@harpbiz.com</a>
+                    </div>
+                    {/* item 2 */}
+                    <div>
+                        <h4>phone</h4>
+                        <p className="max-w-md text-gray-700">407-905-2501</p>
+                    </div>
+                    {/* item 3 */}
+                    <div>
+                        <h4>address</h4>
+                        <p className="max-w-md text-gray-700">Jan Jennings <br />P.O. Box 680451 <br /> Orlando, FL 32868-0451</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </section>
 
 
         </>
