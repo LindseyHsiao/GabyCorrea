31d711fc548b646817fa9ac63f6497a3133f5d6d
Lindsey Sherrill <lindsey350@aol.com>
2023-02-16 11:56:50 -0500

    updated User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,65 @@
-const { Schema, model } = require('mongoose');  //Schema constuctor, model function
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new Schema({
-    userName: {
-        type: String,
-        required: true, 
-        trim: true
-    }, 
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must be a valid email address']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        isPremium: {
+            type: Boolean,
+            default: false
+          },
+          isAdmin: {
+            type: Boolean,
+            default: false
+          }
+
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
-})
+);
 
-//create the User model using the UsersSchema
-const User = model('User', UserSchema);
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
 
-//export the User model
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+
+const User = model('User', userSchema);
+
 module.exports = User;
